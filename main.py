157432d28d0e28f94d24c6003c410ff7e1c5d725
Anees Iqbal <hello@aneesiqbal.ai>
2025-04-25 19:44:58 +0400

    :new: Fix message on boot re Apple Silicon

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import torch
 def main():
     if torch.cuda.is_available():
         print("CUDA is available. Using GPU for processing.")
+    elif torch.mps.is_available():
+        print("MPS is available. Using Apple Silicon for processing.")
     else:
         print("CUDA is not available. Using CPU for processing.")
         
